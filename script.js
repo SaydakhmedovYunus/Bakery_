9375b80c02ea38fe1bb94cb560ac0fdd0f7a4dcd
Yunus_234 <unussaidahmedov@gmail.com>
2024-06-06 21:37:44 +0500

    starting js

---
+++ script.js
@@ -0,0 +1,8 @@
+function popup() {
+    let window = document.getElementsByClassName('popup')[0];
+    window.style.display ="block";
+}
+function closePopup() {
+    let close = document.getElementsByClassName('popup')[0];
+    close.style.display = "none";
+}
